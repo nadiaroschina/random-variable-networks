1f249dccde4f12fb41fdb6b429de8d23dbc6031b
Matvey Smorodin <mtv.smorodin@gmail.com>
2025-04-07 10:10:12 +0300

    Implement threshold calculation

--- compute_correlations.py
+++ compute_correlations.py
@@ -1,19 +1,18 @@
 def compute_pearson_correlation(df):
     edge_weights = {}
-    correlation_matrix = df.corr(method='pearson')
+    correlation_matrix = df.corr(method="pearson")
     for col1 in correlation_matrix.columns:
         for j, col2 in enumerate(correlation_matrix.columns):
             if col1 != col2:
-                    edge_weights[(col1, col2)] = correlation_matrix[col1][j]
+                edge_weights[(col1, col2)] = correlation_matrix[col1].iloc[j]
     return edge_weights
 
 
 def compute_kendall_correlation(df):
     edge_weights = {}
-    correlation_matrix = df.corr(method='kendall')
+    correlation_matrix = df.corr(method="kendall")
     for col1 in correlation_matrix.columns:
         for j, col2 in enumerate(correlation_matrix.columns):
             if col1 != col2:
-                    edge_weights[(col1, col2)] = correlation_matrix[col1][j]
+                edge_weights[(col1, col2)] = correlation_matrix[col1].iloc[j]
     return edge_weights
-
